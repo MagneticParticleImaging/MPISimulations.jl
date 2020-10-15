a4615ed0e75616709e5b9211380da11fa9b7e412
Martin Möddel <m.hofmann@uke.de>
2020-10-15 13:00:33 +0200

    rework excitation fields

--- src/ExcitationFields.jl
+++ src/ExcitationFields.jl
@@ -5,44 +5,21 @@ abstract type ExcitationFields{T} end
 @mustimplement excitationFieldStrength(excitation::ExcitationFields{T},r::SVector{3,T},t::T) where T<:Real
 
 
-## Selection field and 1D excitation field (1D MPS and 1D MPI)
-struct ExcitationField1D{T<:Real,U<:StaticMagneticFields{T},V<:MagneticFields{T}} <: ExcitationFields{T}
+## Selection field and ND sinusoidal excitation field
+struct SinusoidalExcitationField{T<:Real,N,U<:StaticMagneticFields{T}} <: ExcitationFields{T}
     selectionField::U
-    excitationField::V
+    excitationFields::NTuple{N,SinusoidalField{T}}
 end
 
-function excitationFieldStrength(excitation::ExcitationField1D{T},r::SVector{3,T},t::T) where T<:Real
-    Hs = magneticFieldStrength(excitation.selectionField,r,t)
-    H1 = magneticFieldStrength(excitation.excitationField,r,t)
-    return Hs + H1
+function SinusoidalExcitationField(selectionField::StaticMagneticFields{T}, sinusoidalFields...) where T<:Real
+    excitationFields = tuple(sinusoidalFields...)
+    SinusoidalExcitationField(selectionField,excitationFields)
 end
 
-## Selection field and 2D excitation field (2D MPS and 2D MPI)
-struct ExcitationField2D{T<:Real,U<:StaticMagneticFields{T},V<:MagneticFields{T},W<:MagneticFields{T}} <: ExcitationFields{T}
-    selectionField::U
-    excitationField1::V
-    excitationField2::W
-end
-
-function excitationFieldStrength(excitation::ExcitationField2D{T},r::SVector{3,T},t::T) where T<:Real
-    Hs = magneticFieldStrength(excitation.selectionField,r,t)
-    H1 = magneticFieldStrength(excitation.excitationField1,r,t)
-    H2 = magneticFieldStrength(excitation.excitationField2,r,t)
-    return Hs + H1 + H2
-end
-
-## Selection field and 3D excitation field (3D MPS and 3D MPI)
-struct ExcitationField3D{T<:Real,U<:StaticMagneticFields{T},V<:MagneticFields{T},W<:MagneticFields{T},X<:MagneticFields{T}} <: ExcitationFields{T}
-    selectionField::U
-    excitationField1::V
-    excitationField2::W
-    excitationField3::X
-end
-
-function excitationFieldStrength(excitation::ExcitationField3D{T},r::SVector{3,T},t::T) where T<:Real
-    Hs = magneticFieldStrength(excitation.selectionField,r,t)
-    H1 = magneticFieldStrength(excitation.excitationField1,r,t)
-    H2 = magneticFieldStrength(excitation.excitationField2,r,t)
-    H3 = magneticFieldStrength(excitation.excitationField3,r,t)
-    return Hs + H1 + H2 + H3
+function excitationFieldStrength(excitation::SinusoidalExcitationField{T},r::SVector{3,T},t::T) where T<:Real
+    H = magneticFieldStrength(excitation.selectionField,r,t)
+    for sinusoidalField in excitation.excitationFields
+        H += magneticFieldStrength(sinusoidalField,r,t)
+    end
+    return H
 end
--- test/ExcitationFields.jl
+++ test/ExcitationFields.jl
@@ -3,20 +3,19 @@ using Pkg.TOML, StaticArrays
 @testset "ExcitationFields.jl" begin
     params = TOML.parsefile("ExcitationFields.toml")
 	
-    Hs = MPISimulations.initialize(MPISimulations.StaticGradientField,"ExcitationField.selectionField.StaticGradientField",params,Float64)
-    H0 = MPISimulations.initialize(MPISimulations.SinusoidalField,"ExcitationField.excitationField.SinusoidalField",params,Float64)
-    H1 = MPISimulations.initialize(MPISimulations.SinusoidalField,"ExcitationField.excitationField1.SinusoidalField",params,Float64)
-    H2 = MPISimulations.initialize(MPISimulations.SinusoidalField,"ExcitationField.excitationField2.SinusoidalField",params,Float64)
-    H3 = MPISimulations.initialize(MPISimulations.SinusoidalField,"ExcitationField.excitationField3.SinusoidalField",params,Float64)
+    Hs = MPISimulations.initialize(MPISimulations.StaticGradientField,"StaticGradientField",params,Float64)
+    H1 = MPISimulations.initialize(MPISimulations.SinusoidalField,"SinusoidalField1",params,Float64)
+    H2 = MPISimulations.initialize(MPISimulations.SinusoidalField,"SinusoidalField2",params,Float64)
+    H3 = MPISimulations.initialize(MPISimulations.SinusoidalField,"SinusoidalField3",params,Float64)
     r = randn(SVector{3,Float64})
     t = randn(Float64)
     
-    excitation = MPISimulations.initialize(MPISimulations.ExcitationField1D,"ExcitationField",params,Float64)
-    @test MPISimulations.excitationFieldStrength(excitation,r,t) ≈ MPISimulations.magneticFieldStrength(Hs,r,t) + MPISimulations.magneticFieldStrength(H0,r,t)
+    excitation = MPISimulations.SinusoidalExcitationField(Hs,H1)
+    @test MPISimulations.excitationFieldStrength(excitation,r,t) ≈ MPISimulations.magneticFieldStrength(Hs,r,t) + MPISimulations.magneticFieldStrength(H1,r,t)
 
-    excitation = MPISimulations.initialize(MPISimulations.ExcitationField2D,"ExcitationField",params,Float64)
+    excitation = MPISimulations.SinusoidalExcitationField(Hs,H1,H2)
     @test MPISimulations.excitationFieldStrength(excitation,r,t) ≈ MPISimulations.magneticFieldStrength(Hs,r,t) + MPISimulations.magneticFieldStrength(H1,r,t) + MPISimulations.magneticFieldStrength(H2,r,t)
 
-    excitation = MPISimulations.initialize(MPISimulations.ExcitationField3D,"ExcitationField",params,Float64)
+    excitation = MPISimulations.SinusoidalExcitationField(Hs,H1,H2,H3)
     @test MPISimulations.excitationFieldStrength(excitation,r,t) ≈ MPISimulations.magneticFieldStrength(Hs,r,t) + MPISimulations.magneticFieldStrength(H1,r,t) + MPISimulations.magneticFieldStrength(H2,r,t) + MPISimulations.magneticFieldStrength(H3,r,t)
 end
